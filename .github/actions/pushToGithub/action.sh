fb8f2f3854f59a51346bbd8dcd9255c732923b35
James <lim.en.hui.james@gmail.com>
2020-11-14 23:13:10 +0800

    ALL: Begone Travis
    Initial commit to migrate CI/CD operations to GitHub Actions

---
+++ .github/actions/pushToGithub/action.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Pushes stuff to GitHub. Uses environment variables: USERNAME, GITHUB_TOKEN, BRANCH and REPOSITORY
+# This is a separate script to hide the token from the GitHub runner output
+
+git config user.name "CodingIndex Deploy"
+git config user.email "deploy@codingindex.xyz"
+git add .
+git commit -m "Deployment from GitHub runner"
+git push https://${USERNAME}:${GITHUB_TOKEN}@github.com/${REPOSITORY} master:${BRANCH}
